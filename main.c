4479286a15095de2ac17e35842ee22321839109c
Afenndi <cestmoi.afenndi.dj@mail.ru>
2022-06-12 00:14:27 +0400

    main.c changing switch

--- main.c
+++ main.c
@@ -34,6 +34,21 @@ int main(int argc, char const *argv[]){
         "Choose initial board state: "
     );  getchar(); // to consume leading space
     ini_opt = getchar();
+
+    if(ini_opt == '1'){
+        board = randomize_board(board);
+    } else if(ini_opt == '2'){
+        board.cell[2][1] = ALIVE;
+        board.cell[2][2] = ALIVE;
+        board.cell[2][3] = ALIVE;
+    } else if(ini_opt == '3'){
+        board.cell[1][1] = ALIVE;
+        board.cell[2][2] = ALIVE;
+        board.cell[2][3] = ALIVE;
+        board.cell[3][1] = ALIVE;
+        board.cell[3][2] = ALIVE;
+    }
+
     switch (ini_opt){
         case '1':
             board = randomize_board(board);
