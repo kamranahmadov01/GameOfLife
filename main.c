d2c0c722f469548240e0df37989361280f82767f
Afenndi <cestmoi.afenndi.dj@mail.ru>
2022-06-11 23:13:05 +0400

    main.c update

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
+    char board_type_opt, ini_opt, if_opt;
+    printf(
+        "Welcome to Conway's game of life!\n"
+        "[1] Clipped\n"
+        "[2] Circular\n"
+        "Choose board type: "
+    );
 
 
     return 0;
